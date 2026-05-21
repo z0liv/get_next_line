e5e44356907ade03eabfd8106757c6311106e0e6
Oliver Marquez <omarquez@c2r1s4.42urduliz.com>
2026-05-21 15:37:33 +0200

    initial commit to initialize the project with some files

---
+++ README.md
@@ -0,0 +1,7 @@
+*Este proyecto ha sido creado como parte del currículo de 42 por omarquez*
+
+# GET_NEXT_LINE:
+## DESCRIPCION:
+## INSTRUCCIONES:
+## RECURSOS:
+- [geeksforgeeks]https://www.geeksforgeeks.org/c/static-variables-in-c/ La use de introduccion al concepto de variables estaticas.
---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omarquez <omarquez@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 15:29:50 by omarquez          #+#    #+#             */
+/*   Updated: 2026/05/21 15:34:05 by omarquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+#endif
